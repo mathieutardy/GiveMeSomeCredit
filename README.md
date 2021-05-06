a2f22a49c32b0ccaa8d85d03c749f19c062755f6
mathieutardy <mathieutardy97@gmail.com>
2021-05-06 17:47:57 +0800

    read me and debugging

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # GiveMeSomeCredit
+
+- Tell us how you validate your model, which, and why you chose such evaluation technique(s).
+I used a 5-fold cross validation method. The advantage of this method is that you do not lose any training data in the process in contrast with a classical train_test_split technique.
+
+The disadvantage of this technique is raises the possibility of you overfitting the training data.
+
+- What is AUC? Why do you think AUC was used as the evaluation metric for such a problem? What are other metrics that you think would also be suitable for this competition?
+
+AUC - ROC curve is a performance measurement for the classification problems at various threshold settings. The ROC curve is plotted with True Positive Rate (TPR) against the False Positive Rate (FPR) where TPR is on the y-axis and FPR is on the x-axis.
+
+AUC stands for Area under the Curve. The curve in question is the ROC curve. The area is the integral below the ROC curve. Its maximum value is 1 and minimum value is 0. When the AUC is 1, TPR is equal to 1 and FPR is equal to 0. This means that the model makes perfect predictions.
+
+The advantage of the AUC- ROC curve is that it evaluates the model for each probability threshold that the model outputs.
+
+An alternative evaluation metric could have been to plot a precision and recall curve based on the F1-score. Usually, this metric is better suited for unbalanced datasets in terms of their dependent variable y.
+
+- What insight(s) do you have from your model? What is your preliminary analysis of the given dataset?
+
+By plotting the most importance features, we notice that 
+
+- Can you get into the top 100 of the private leaderboard, or even higher?
+
+No. I have a 
--- credit_scorer.py
+++ credit_scorer.py
@@ -1,60 +1,69 @@
+# Useful packages
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import GridSearchCV,RandomizedSearchCV, cross_val_score
-from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
 import pickle
 
-# Packages for model training
+# Models
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import (
     RandomForestClassifier,
     AdaBoostClassifier,
     GradientBoostingClassifier,
-    ExtraTreesClassifier
+    ExtraTreesClassifier,
 )
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
+# Model Preprocessing and validation
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, cross_val_score
+from sklearn.preprocessing import StandardScaler
+
+# Evaluation metric
 from sklearn.metrics import roc_auc_score
 
-class CreditScorer:
 
+class CreditScorer:
     def __init__(self):
-        self.X, self.y = self.load_data('cs-training.csv')
+        self.X, self.y = self.load_data("cs-training.csv")
         self.classifier = None
-        
-    def load_data(self,path, train=True):
+
+    def load_data(self, path, train=True):
         df = pd.read_csv(path)
-        # Replace missing values with mean of of column (applies to )
+        # Replace missing values with mean of of column (applies to MonthlyIncome and MonthlyIncome).
+        # It makes sense to replace by mean for those two variables
         df = df.fillna(df.mean())
-        self.X = df.drop(['SeriousDlqin2yrs','Unnamed: 0'], axis=1).values
+
+        # Preprocessing
+        self.X = df.drop(["SeriousDlqin2yrs", "Unnamed: 0"], axis=1).values
         scaler = StandardScaler().fit(self.X)
         self.X = scaler.transform(self.X)
+
         if train == True:
             self.y = df["SeriousDlqin2yrs"].values
-            return self.X,self.y
+            return self.X, self.y
         else:
             id = df["Unnamed: 0"].values
-            return X,id
-
+            return self.X, id
 
     def evaluate_classifiers(self):
+        """ Evaluation of 9 classifiers """
+
         classifiers = [
-            SVC(),
-            LogisticRegression(),
-            DecisionTreeClassifier(),
-            RandomForestClassifier(),
-            AdaBoostClassifier(),
-            GradientBoostingClassifier(),
+            # SVC(),
+            # LogisticRegression(),
+            # DecisionTreeClassifier(),
+            # RandomForestClassifier(),
+            # AdaBoostClassifier(),
+            # GradientBoostingClassifier(),
             XGBClassifier(),
-            ExtraTreesClassifier(),
-            LGBMClassifier(),
-            ] 
+            # ExtraTreesClassifier(),
+            # LGBMClassifier(),
+        ]
 
         lst = []
         best_clf = None
@@ -62,7 +71,8 @@ class CreditScorer:
 
         for clf in tqdm(classifiers):
             clf.fit(self.X, self.y)
-            auc = cross_val_score(clf, self.X, self.y, cv=5, scoring = 'roc_auc').mean()
+            auc = cross_val_score(clf, self.X, self.y,
+                                  cv=5, scoring="roc_auc").mean()
             if auc > best_auc:
                 best_auc = auc
                 best_clf = clf
@@ -74,61 +84,91 @@ class CreditScorer:
         print(results)
 
         self.classifier = best_clf
+    
+    def save_model(self, path):
+        pickle.dump(self.classifier, open(path, "wb"))
 
-    def save_model(self,path):
-        pickle.dump(self.classifier,open( path, "wb"))
+    def train_classifier(self, path, classifier):
+        """ Train a specific classifier
 
-    def train_classifier(self,path):
-        clf = self.classifier
-        X,y = self.load_data('cs-training.csv')
-        clf.fit(X, y)
-        auc = cross_val_score(clf, X, y, cv=5, scoring = 'roc_auc').mean()
-        print('AUC is: {0:.04f}'.format(auc))
-        save_model(clf,path)
+        Args:
+            path (string): path of where to save the model
+        """
+        self.classifier = classifier
+        self.classifier.fit(self.X, self.y)
+        auc = cross_val_score(self.classifier, self.X, self.y, cv=5,
+                              scoring="roc_auc").mean()
+        print("AUC is: {0:.04f}".format(auc))
+        self.save_model(path)
 
     def hyperparameter_tuning(self):
+        """ Using Random Search CV we evaluable a large range of possible hyperparameters """
 
+        # Set manually some parameters
         self.classifier.n_estimators = 300
         self.classifier.learning_rate = 0.01
-        self.classifier.objective = 'binary'
+        self.classifier.objective = "binary"
 
         random_params = {
-            'max_depth' : [6,7,8,9,10],
-            'min_child_samples': [5,10, 15,20,25,30,35,40],
-            'num_leaves': [10,20,30,40,50,60],
-            }
+            "max_depth": [6, 7, 8, 9, 10],
+            "min_child_samples": [5, 10, 15, 20, 25, 30, 35, 40],
+            "num_leaves": [10, 20, 30, 40, 50, 60],
+        }
 
         clf = self.classifier
-        random_lgb =  RandomizedSearchCV(estimator=clf, param_distributions=random_params,scoring='roc_auc',cv=5,n_jobs=-1)
-        random_lgb.fit(self.X,self.y)
-        print('Best parameters are: {}'.format(random_lgb.best_params_))
-        print('Best score is: {0:.4f}'.format(random_lgb.best_score_))
+        random_lgb = RandomizedSearchCV(
+            estimator=clf,
+            param_distributions=random_params,
+            scoring="roc_auc",
+            cv=5,
+            n_jobs=-1,
+        )
+        random_lgb.fit(self.X, self.y)
+        print("Best parameters are: {}".format(random_lgb.best_params_))
+        print("Best score is: {0:.4f}".format(random_lgb.best_score_))
 
         self.classifier = random_lgb.best_estimator_
 
     def funetuning_model(self):
+        """ Using GridSearchCV we finetune our hyperparameter space after after having applied a random search """
+
         grid_params = {
-            'min_child_samples': [30,35,40,45],
-            'num_leaves': [50,55,60,65],
-            }
+            "min_child_samples": [30, 35, 40, 45],
+            "num_leaves": [50, 55, 60, 65],
+        }
 
         clf = self.classifier
 
-        grid_lgb =  GridSearchCV(estimator=clf, param_grid=grid_params,scoring='roc_auc',cv=5,n_jobs=-1)
+        grid_lgb = GridSearchCV(
+            estimator=clf, param_grid=grid_params, scoring="roc_auc", cv=5, n_jobs=-1
+        )
 
-        grid_lgb.fit(self.X,self.y)
-        print('Best parameters are: {}'.format(grid_lgb.best_params_))
-        print('Best score is: {0:.4f}'.format(grid_lgb.best_score_))
+        grid_lgb.fit(self.X, self.y)
+        print("Best parameters are: {}".format(grid_lgb.best_params_))
+        print("Best score is: {0:.4f}".format(grid_lgb.best_score_))
 
         self.classifier = grid_lgb.best_estimator_
 
         self.save_model("./models/best_model.pkl")
-    
-    def load_model(self,path):
-        self.classifer = pickle.load(open(path, "rb" ) )
 
-    def submit_kaggle(self,path):
-        X_test,id = self.load_data('cs-test.csv', train=False)
+    def load_model(self, path):
+        self.classifer = pickle.load(open(path, "rb"))
+    
+    def plot_feature_importance(self):
+        df = pd.read_csv('cs-training.csv')
+        columns = df.drop(['SeriousDlqin2yrs','Unnamed: 0'],axis=1).columns
+        plt.barh(columns,self.classifier.feature_importances_)
+        plt.show()
+
+    def submit_kaggle(self, path):
+        """Creates a submission file in the correct format for the kaggle competition
+
+        Args:
+            path (string): path and filename describing where the file will be saved in
+        """
+        X_test, id = self.load_data("cs-test.csv", train=False)
         predictions = self.classifier.predict_proba(X_test)[:, 1]
-        submission = pd.DataFrame(list(zip(id, predictions)), columns = ['Id', 'Probability'])
-        submission.to_csv(path,index=False)
+        submission = pd.DataFrame(
+            list(zip(id, predictions)), columns=["Id", "Probability"]
+        )
+        submission.to_csv(path, index=False)
